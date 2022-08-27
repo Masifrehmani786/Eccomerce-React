195b3a605b51ecfe38e18b711ac9b4585d394f55
Muhammad Asif Bashir <masifrehmani.itroots@gmail.com>
2022-08-27 10:53:24 +0500

    Here is awesome eccomerce react app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <h4 className='text-center color-red'>Created by M Asif Bashir</h4>
+        </div>
+    )
+}
+
+export default Footer
